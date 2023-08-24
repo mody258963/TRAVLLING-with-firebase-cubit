6b98701c073b6a8bffaccb9da3c384f6b84188ed
mody258963 <122531523+mody258963@users.noreply.github.com>
2023-08-24 21:04:14 +0300

    8/24
    2

--- lib/prestation_layer/screns/OTP.dart
+++ lib/prestation_layer/screns/OTP.dart
@@ -8,8 +8,37 @@ class OPTverify extends StatefulWidget {
 }
 
 class _OPTverifyState extends State<OPTverify> {
+  Widget _TextoftheVerfy() {
+    return Column(
+      children: [
+        Text(
+          "Verify your phone number",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        Text("Enter your 6 digit code numbers sent to you at ##number##")
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                  'assets/fonts/image/photo-1517950908067-2d4181c93e31.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
   }
 }
