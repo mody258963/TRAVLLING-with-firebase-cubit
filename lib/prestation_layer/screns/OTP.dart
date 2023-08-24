e14efc079bd35a334b2f57113d4aeda386a78cdb
mody258963 <122531523+mody258963@users.noreply.github.com>
2023-08-24 22:52:34 +0300

    8/243
    3

--- lib/prestation_layer/screns/OTP.dart
+++ lib/prestation_layer/screns/OTP.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:traelling_app/costanse/colors.dart';
 
 class OPTverify extends StatefulWidget {
   const OPTverify({super.key});
@@ -8,20 +10,84 @@ class OPTverify extends StatefulWidget {
 }
 
 class _OPTverifyState extends State<OPTverify> {
+
+
+  
+Widget _Pincodetextfield(){
+  double width = MediaQuery.of(context).size.width;
+    double hight = MediaQuery.of(context).size.height;
+  return Container(
+    width: width * 0.90,
+    child: PinCodeTextField(
+    length: 6,
+    
+    enablePinAutofill: true,
+    keyboardType: TextInputType.phone,
+    obscureText: false,
+    animationType: AnimationType.fade,
+    pinTheme: PinTheme(
+      shape: PinCodeFieldShape.box,
+      borderRadius: BorderRadius.circular(5),
+      fieldHeight: 50,
+      fieldWidth: 40,
+      selectedColor: MyColors.white,
+      inactiveColor: Colors.black,
+      selectedFillColor: Colors.black,
+      inactiveFillColor:MyColors.white ,
+      disabledColor: MyColors.white,
+      activeColor: MyColors.white,
+      activeFillColor: Colors.white,
+
+    ),
+    animationDuration: Duration(milliseconds: 300),
+    backgroundColor: Colors.transparent,
+    enableActiveFill: true,
+    
+    onCompleted: (v) {
+      print("Completed");
+    },
+    onChanged: (value) {
+      print(value);
+      setState(() {
+        
+      });
+    },
+    beforeTextPaste: (text) {
+      print("Allowing to paste $text");
+      //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+      //but you can show anything you want here, like your pop up saying wrong paste format or etc
+      return true;
+    }, appContext: context,
+  ),
+  );
+}
+
+
+
   Widget _TextoftheVerfy() {
+    double width = MediaQuery.of(context).size.width;
+    double hight = MediaQuery.of(context).size.height;
     return Column(
       children: [
-        Text(
-          "Verify your phone number",
-          style: TextStyle(fontWeight: FontWeight.bold),
+        Padding(
+          padding:  EdgeInsets.only(left:  width * 0.05),
+          child: Text(
+            "Verify your phone number",
+            style: TextStyle(fontWeight: FontWeight.bold,fontSize: width * 0.08,color: MyColors.white),
+          ),
         ),
-        Text("Enter your 6 digit code numbers sent to you at ##number##")
+        Padding(
+          padding:  EdgeInsets.only(left:width * 0.05,right: width * 0.05 ),
+          child: Text("Enter your 6 digit code numbers sent to you at ##number##",style:TextStyle(fontWeight: FontWeight.bold,fontSize: width * 0.04,color: MyColors.white) ,),
+        )
       ],
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double hight = MediaQuery.of(context).size.height;
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Poppins'),
       home: Scaffold(
@@ -35,7 +101,14 @@ class _OPTverifyState extends State<OPTverify> {
             ),
           ),
           child: Column(
-            children: [],
+            children: [
+              SizedBox(
+                height: hight * 0.10 ,
+              ),
+              _TextoftheVerfy(),
+              SizedBox(height: hight * 0.05,),
+              _Pincodetextfield(),
+            ],
           ),
         ),
       ),
